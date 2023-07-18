cc01810375aec7418c85a2e91e4d55c6c594c07e
RahmaaEsmail <rahmaaesmail26@gmail.com>
2023-07-18 21:31:52 +0300

    Upload Task

---
+++ data.js
@@ -0,0 +1,20 @@
+const translations = {
+    home_heading: {
+        ar: "مقال قابل للترجمة",
+        en: "Translatable Article"
+    },
+    home_paragraph: {
+        ar: "اختر لغة",
+        en: "Choose a language"
+    },
+    home_text: {
+        ar: "اهلا بك , اسمى رحمه اسماعيل , انا مطورة مواقع مصري ولدت فى عام 2001.انا استمتع بانشاء مواقع رائعة باستخدام جافاسكريبت",
+        en: "Hello there, My name is Rahma Esmail, I'm an egyptian web developer who was born in 2001.I enjoy creating cool websites with JavaScript."
+    },
+    home_link: {
+        ar: "اقرأ المزيد",
+        en: "Read More"
+    }
+}
+
+export default translations
---
+++ index.js
@@ -0,0 +1,33 @@
+import data from './data.js';
+
+const translateText = (lang)=> {
+    const container = document.querySelector(".container");
+    const primaryHeading = document.querySelector("h1");
+    const headingParagraph = document.querySelector(".heading-paragraph");
+    const headingText = document.querySelector("article p");
+    const link = document.querySelector("a");
+
+    if(lang == 'ar')
+     container.dir = "rtl";
+    else 
+        container.dir = "ltr";
+    
+    primaryHeading.innerHTML = data.home_heading[lang];
+    headingParagraph.innerHTML = data.home_paragraph[lang];
+    headingText.innerHTML = data.home_text[lang];
+    link.innerHTML = data.home_link[lang];
+}
+
+
+const init = ()=>{
+    const btns = document.querySelectorAll("button");
+    btns.forEach(btn => {
+        btn.addEventListener("click", (e) => {
+            if (e.target.innerHTML == "العربية")
+                translateText('ar');
+            else
+                translateText('en')
+        })
+    })
+}
+init()
